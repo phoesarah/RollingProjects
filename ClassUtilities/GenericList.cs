aec2dbc50e59fa5999b5297ca91fc68790b3b314
Andrew Benz <andorbal@gmail.com>
2015-05-15 19:45:32 -0500

    This doesn't work, but it has the generic linq stuff

---
+++ ClassUtilities/GenericList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassUtilities
+{
+    public class GenericList<T> 
+    {
+        private T[] innerList;
+
+        public GenericList()
+        {
+            innerList = new T[0];
+        }
+
+        public GenericList(T[] passedInArray)
+        {
+            innerList = new T[passedInArray.Length];
+            passedInArray.CopyTo(innerList, 0);
+        }
+    }
+}
---
+++ LinqyGoodness/Examples.cs
@@ -0,0 +1,62 @@
+﻿using ClassUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqyGoodness
+{
+    public class Examples
+    {
+        public void Example()
+        {
+            var doubleList = new List<double> { 3.2, 6.1 };
+
+            var stringList = new List<string> { "Foo", "Bar", "Baz" };
+            //var stringList = new List<int> { 3, 4, 5 };
+
+            stringList
+                .Select(x => x)
+                .GroupBy(x => x.First());
+
+            var orderList = new List<Order> { new SalesOrder(), new PurchaseOrder() };
+        }
+
+        public void PageExample()
+        {
+            var somethiign = new Dictionary<int, Dictionary<string, Tuple<double, decimal>>>()
+
+
+            int pageSize = 25;
+            int currentPage = 2;
+            var intList = Enumerable.Range(0, 100).ToList();
+
+            var pageOfList = Page(intList, 2, 25);
+            //intList.Skip(25 * (currentPage - 1)).Take(pageSize);
+
+            var stringList = new List<string>();
+            Page(stringList, 2, 25);
+        }
+
+        public IEnumerable<T> Page<T>(IEnumerable<T> list, int currentPage, int pageSize)
+        {
+            return list.Skip(pageSize * (currentPage - 1)).Take(pageSize);
+        }
+    }
+
+    public class Order
+    {
+
+    }
+
+    public class SalesOrder : Order
+    {
+
+    }
+
+    public class PurchaseOrder : Order
+    {
+
+    }
+}
